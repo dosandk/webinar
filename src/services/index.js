8f43eb593228923133f2cb11b872b84229dffd43
unknown <nazarkoo4435@gmail.com>
2018-11-19 12:12:38 +0200

    added sessionStorage service

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 import * as cookieAdapter from './cookie/adapter';
+import * as sessionStorageAdapter from './session-storage/adapter';
 import * as localStorageAdapter from './local-storage/adapter';
 import {mLabService} from './mLab';
 
 const adapters = {
   cookie: cookieAdapter,
+  sessionStorage: sessionStorageAdapter,
   localStorage: localStorageAdapter,
   mLab: mLabService
 };
---
+++ src/services/session-storage/adapter.js
@@ -0,0 +1,28 @@
+import { sessionStorageService } from './index';
+
+export const remove = value => {
+  const currentCookie = sessionStorageService.read('todo');
+  const cookie = currentCookie
+    .split('&')
+    .filter(item => item !== value)
+    .join('&');
+
+  sessionStorageService.write('todo', cookie);
+};
+
+export const save = value => {
+  const currentCookie = sessionStorageService.read('todo');
+  const currentValue = currentCookie ? `${currentCookie}&` : '';
+
+  sessionStorageService.write('todo', `${currentValue}${value}`);
+};
+
+export const read = () => {
+  const result = sessionStorageService.read('todo');
+
+  return result ? Promise.resolve(result.split('&')) : Promise.resolve([]);
+};
+
+export const removeAll = () => {
+  sessionStorageService.remove('todo');
+};
