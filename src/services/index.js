e789db0960a53ce608223e692f05451774ab0835
unknown <nazarkoo4435@gmail.com>
2018-11-22 22:44:11 +0200

    implemented indexedDB service

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,12 @@
 import * as cookieAdapter from './cookie/adapter';
 import * as sessionStorageAdapter from './session-storage/adapter';
 import * as localStorageAdapter from './local-storage/adapter';
+import * as IndexedDBAdapter from './index-db/adapter';
 import {mLabService} from './mLab';
 
 const adapters = {
   cookie: cookieAdapter,
+  IndexedDB: IndexedDBAdapter,
   sessionStorage: sessionStorageAdapter,
   localStorage: localStorageAdapter,
   mLab: mLabService
