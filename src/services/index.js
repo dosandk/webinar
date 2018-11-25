88d6e92a1c9723a51177933c103f9e7d357e70dd
vetoshk1n <olvetoshkin@gmail.com>
2018-11-25 22:31:03 +0200

    add session-storage service

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,13 @@
 import * as cookieAdapter from './cookie/adapter';
 import * as localStorageAdapter from './local-storage/adapter';
 import {mLabService} from './mLab';
+import * as sessionStorageAdapter from './session-storage/adapter';
 
 const adapters = {
   cookie: cookieAdapter,
   localStorage: localStorageAdapter,
-  mLab: mLabService
+  mLab: mLabService,
+  sessionStorage: sessionStorageAdapter
 };
 
 export const getAdapter = name => {
