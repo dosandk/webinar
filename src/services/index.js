186e42d71b024ae94fcfe2f1b6cf1a9948a2ddcb
ya_ron <dr.likle@gmail.com>
2018-11-26 14:02:52 +0200

    Added index-db service

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,13 @@
 import * as cookieAdapter from './cookie/adapter';
 import * as localStorageAdapter from './local-storage/adapter';
+import * as indexedDBAdapter from './index-db/adapter';
 import {mLabService} from './mLab';
 
 const adapters = {
   cookie: cookieAdapter,
   localStorage: localStorageAdapter,
-  mLab: mLabService
+  mLab: mLabService,
+  indexedDB: indexedDBAdapter
 };
 
 export const getAdapter = name => {
